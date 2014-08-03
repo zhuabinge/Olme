e12e713f07ad840d4124f5fd184ccf26f02f1240
zhuabinge <739699743@qq.com>
2014-08-03 13:32:35 +0800

    初始框架

---
+++ app/src/main/java/com/kubang/olme/activity/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.kubang.olme.activity;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.kubang.olme.api.OlmeApi;
+import com.kubang.olme.domain.TUser;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.rest.RestService;
+import org.springframework.web.client.HttpStatusCodeException;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends Activity {
+
+    @ViewById(R.id.et_username)
+    EditText etUsername;
+
+    @ViewById(R.id.et_password)
+    EditText etPassword;
+
+    @ViewById(R.id.btn_login)
+    Button btn;
+
+    @RestService
+    OlmeApi olmeApi;
+
+    String username;
+
+    String password;
+
+    @Click(R.id.btn_login)
+    void buttonIsClicked(){
+        username = etUsername.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        login(username,password);
+    }
+
+    @Background
+    void login(String username,String password) {
+        try {
+            TUser user = olmeApi.login(username,password);
+            System.out.println(user.toString());
+        }catch (HttpStatusCodeException e){
+            showErroResult(e.getStatusCode().value());
+        }
+    }
+
+    /**
+     * 展示http请求异常结果
+     * @param requestCode
+     */
+    @UiThread
+    void showErroResult(int requestCode){
+        if(requestCode == 404){
+
+        }else{
+        }
+    }
+
+    private boolean vilidata(String username,String password) {
+        if("".equals(username))
+            return false;
+        if("".equals(password))
+            return false;
+        return true;
+    }
+}
--- app/src/main/java/com/kubang/olme/activity/MainActivity.java
+++ app/src/main/java/com/kubang/olme/activity/MainActivity.java
@@ -1,77 +1,94 @@
 package com.kubang.olme.activity;
 
+import java.util.ArrayList;
+import java.util.List;
 import android.app.Activity;
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-import com.kubang.olme.api.OlmeApi;
-import com.kubang.olme.domain.TUser;
+import com.kubang.olme.adapter.Myadapter;
+import com.kubang.olme.dataSource.DataSource;
+import com.kubang.olme.onClickListener.MyOnClickListener;
+import com.kubang.olme.onPageChangeListener.MyOnPageChangeListener;
 
-import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Background;
-import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.Extra;
-import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
-import org.androidannotations.annotations.rest.RestService;
-import org.apache.http.impl.client.TunnelRefusedException;
-import org.springframework.web.client.HttpStatusCodeException;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
-    @ViewById(R.id.et_username)
-    EditText etUsername;
+	@ViewById(R.id.viewpage1)
+	ViewPager viewpager;
 
-    @ViewById(R.id.et_password)
-    EditText etPassword;
+	@ViewById(R.id.searchbt)
+	ImageView searchbt;
 
-    @ViewById(R.id.btn_login)
-    Button btn;
+	@ViewById(R.id.morebt)
+	ImageView morebt;
 
-    @RestService
-    OlmeApi olmeApi;
+	@Bean
+	DataSource data;
 
-    String username;
+	private TextView head, classes, discuss, personal, recommend;
+	private List<TextView> textviewList;
+	private SimpleAdapter adapter;
+	private List<View> list = null;
+    private int currIndex = 0;// 当前页卡编号
+	
+	
+	@AfterViews
+	void init() {
+		initTextView();
+		initViewPager();
+	}
 
-    String password;
+	public void initTextView() {
+		head = (TextView) findViewById(R.id.headpage);
+		classes = (TextView) findViewById(R.id.classpage);
+		discuss = (TextView) findViewById(R.id.discusspage);
+		personal = (TextView) findViewById(R.id.personalpage);
+		recommend = (TextView) findViewById(R.id.recommendpage);
+		textviewList = new ArrayList<TextView>();
+		textviewList.add(head);
+		textviewList.add(classes);
+		textviewList.add(discuss);
+		textviewList.add(personal);
+		textviewList.add(recommend);
+		head.setBackgroundColor(android.graphics.Color.parseColor("#00BFFF")); //设置起始背景色
+		head.setOnClickListener(new MyOnClickListener(viewpager, textviewList, 0));
+		classes.setOnClickListener(new MyOnClickListener(viewpager, textviewList, 1));
+		discuss.setOnClickListener(new MyOnClickListener(viewpager, textviewList, 2));
+		personal.setOnClickListener(new MyOnClickListener(viewpager, textviewList, 3));
+		recommend.setOnClickListener(new MyOnClickListener(viewpager,textviewList, 4));
+	}
 
-    @Click(R.id.btn_login)
-    void buttonIsClicked(){
-        username = etUsername.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
-        login(username,password);
-    }
-
-    @Background
-    void login(String username,String password) {
-        try {
-            TUser user = olmeApi.login(username,password);
-            System.out.println(user.toString());
-        }catch (HttpStatusCodeException e){
-            showErroResult(e.getStatusCode().value());
-        }
-    }
-
-    /**
-     * 展示http请求异常结果
-     * @param requestCode
-     */
-    @UiThread
-    void showErroResult(int requestCode){
-        if(requestCode == 404){
-
-        }else{
-        }
-    }
-
-    private boolean vilidata(String username,String password) {
-        if("".equals(username))
-            return false;
-        if("".equals(password))
-            return false;
-        return true;
-    }
+	public void initViewPager() {
+		LayoutInflater inflaters = getLayoutInflater();
+		ListView listView1 = (ListView) (inflaters.inflate(R.layout.tab1, null))
+				.findViewById(R.id.listview);
+		adapter = new SimpleAdapter(this, data.getDataSource(), R.layout.item,
+				new String[] { "img", "username", "phone", "address" },
+				new int[] { R.id.img, R.id.username, R.id.phone, R.id.address });
+		listView1.setAdapter(adapter);
+		View view2 = inflaters.inflate(R.layout.tab2, null);
+		View view3 = inflaters.inflate(R.layout.tab3, null);
+		View view4 = inflaters.inflate(R.layout.tab4, null);
+		View view5 = inflaters.inflate(R.layout.tab5, null);
+		list = new ArrayList<View>();
+		list.add(listView1);
+		list.add(view2);
+		list.add(view3);
+		list.add(view4);
+		list.add(view5);
+		viewpager.setAdapter(new Myadapter(list));
+		viewpager.setOnPageChangeListener(new MyOnPageChangeListener(textviewList,currIndex));
+	}
 }
+
